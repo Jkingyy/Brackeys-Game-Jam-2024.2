b70863cc9975dd0dfad29e8efd4b4ac84d5b8ec1
Jkingyy <jkingy2000@gmail.com>
2024-09-09 20:03:02 +0100

    Created basic pollen collection

---
+++ Brackey's Game Jam 2024.2/Assets/Scripts/Player/Collection.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Collection : MonoBehaviour
+{
+    [SerializeField] TMP_Text _collectionText;
+
+    private int collectionCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Collect")){
+            collectionCount++;
+            UpdateUI();
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void UpdateUI(){
+        _collectionText.text = collectionCount.ToString();
+    }
+}
