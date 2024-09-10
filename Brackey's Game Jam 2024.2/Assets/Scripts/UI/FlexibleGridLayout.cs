e53fb757027bd0dfeab07688cf56746f25db39b6
Jkingyy <jkingy2000@gmail.com>
2024-09-10 21:45:10 +0100

    Created Menu System
    Created the title menu, options and credits canvases but forgot to do it in it own branch fml

---
+++ Brackey's Game Jam 2024.2/Assets/Scripts/UI/FlexibleGridLayout.cs
@@ -0,0 +1,109 @@
+﻿/* FlexibleGridLayout.cs
+ * From: Game Dev Guide - Fixing Grid Layouts in Unity With a Flexible Grid Component
+ * Created: June 2020, NowWeWake
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlexibleGridLayout : LayoutGroup
+{    
+    public enum FitType
+    {
+        Uniform,
+        Width,
+        Height,
+        FixedRows,
+        FixedColumns
+    }
+
+    [Header("Flexible Grid")]
+    public FitType fitType = FitType.Uniform;
+
+    public int rows;
+    public int columns;
+    public Vector2 cellSize;
+    public Vector2 spacing;
+
+    public bool fitX;
+    public bool fitY;
+
+    public override void CalculateLayoutInputHorizontal()
+    {
+        base.CalculateLayoutInputHorizontal();
+
+        if (fitType == FitType.Width || fitType == FitType.Height || fitType == FitType.Uniform)
+        {
+            float squareRoot = Mathf.Sqrt(transform.childCount);
+            rows = columns = Mathf.CeilToInt(squareRoot);
+            switch (fitType)
+            {
+                case FitType.Width:
+                    fitX = true;
+                    fitY = false;
+                    break;
+                case FitType.Height:
+                    fitX = false;
+                    fitY = true;
+                    break;
+                case FitType.Uniform:
+                    fitX = fitY = true;
+                    break;
+            }
+        }
+
+        if (fitType == FitType.Width || fitType == FitType.FixedColumns)
+        {
+            rows = Mathf.CeilToInt(transform.childCount / (float)columns);
+        }
+        if (fitType == FitType.Height || fitType == FitType.FixedRows)
+        {
+            columns = Mathf.CeilToInt(transform.childCount / (float)rows);
+        }
+        
+
+        float parentWidth = rectTransform.rect.width;
+        float parentHeight = rectTransform.rect.height;
+
+        float cellWidth = parentWidth / (float)columns - ((spacing.x / (float)columns) * (columns - 1))
+            - (padding.left / (float)columns) - (padding.right / (float)columns);
+        float cellHeight = parentHeight / (float)rows - ((spacing.y / (float)rows) * (rows - 1))
+            - (padding.top / (float)rows) - (padding.bottom / (float)rows); ;
+
+        cellSize.x = fitX ? cellWidth : cellSize.x;
+        cellSize.y = fitY ? cellHeight : cellSize.y;
+
+        int columnCount = 0;
+        int rowCount = 0;
+
+        for (int i = 0; i < rectChildren.Count; i++)
+        {
+            rowCount = i / columns;
+            columnCount = i % columns;
+
+            var item = rectChildren[i];
+
+            var xPos = (cellSize.x * columnCount) + (spacing.x * columnCount) + padding.left;
+            var yPos = (cellSize.y * rowCount) + (spacing.y * rowCount) + padding.top;
+
+            SetChildAlongAxis(item, 0, xPos, cellSize.x);
+            SetChildAlongAxis(item, 1, yPos, cellSize.y);
+
+        }
+    }
+
+    public override void CalculateLayoutInputVertical()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void SetLayoutVertical()
+    {
+        //throw new System.NotImplementedException();
+    }
+}
