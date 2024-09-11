753797c7ffd64b74d6ae9bee2d71b4317464838f
MyPing0 <myping0yt@gmail.com>
2024-09-11 15:41:02 -0400

    Weather Effects

---
+++ Brackey's Game Jam 2024.2/Assets/Scripts/Weather/Lightning/LightningFlash.cs
@@ -0,0 +1,62 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class LightningFlash : MonoBehaviour
+{
+	[SerializeField] ParticleSystem ps;
+	[SerializeField] Animator lightAnimator;
+	[SerializeField] string animationName = "Lightning Flash";
+	[SerializeField] float playChance = 1f;
+
+	private bool checking = true;
+	
+
+	void Start()
+	{
+		if (ps == null)
+		{
+			Debug.LogError("ParticleSystem reference is missing!");
+			return;
+		}
+	}
+
+	void Update()
+	{
+		if (checking && ps.particleCount > 0)
+		{
+			// Start the coroutine to wait for the particle system duration
+			StartCoroutine(CheckParticleSystem());
+		}
+	}
+	
+	System.Collections.IEnumerator CheckParticleSystem()
+	{
+		checking = false;
+
+		// Trigger the animation if a particle is alive
+		if (Random.value < playChance) PlayLightningFlash();
+
+		// Wait for the particle system's duration
+		print("Waiting for " + ps.main.startLifetime.constantMax + " seconds.");
+		yield return new WaitForSeconds(ps.main.startLifetime.constantMax);
+
+		// After waiting, check again in the next Update cycle
+		checking = true;
+	}
+
+	void PlayLightningFlash()
+	{
+		print("Playing lightning flash!");
+		// Play the animation using the trigger
+		lightAnimator.Play(animationName);
+	}
+	
+	private void OnValidate()
+    {
+        if (ps == null || lightAnimator == null)
+		{
+			Debug.LogError("ParticleSystem or Animator reference is missing on the " + gameObject.name + "GameObject!");
+		}
+    }
+}
